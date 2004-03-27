2d0c1b1d0591eceae8563df186a94c89571665c2
Thomas Baum <thomas.baum@arcor.de>
2004-03-27 11:04:58 +0000

    Auslagerung von Funktionalitäten nach Heb.js

--- tinyheb/erfassung/krankenkassen.js
+++ tinyheb/erfassung/krankenkassen.js
@@ -1,37 +1,12 @@
-  function loeschen() {
+function loeschen() {
   open("krankenkassenerfassung.pl","_top");
-  };
+};
 
-  function kassesuchen(name,ik,formular) {
+function kassesuchen(name,ik,formular) {
   // öffnet Fenster in dem eine Krankenkasse ausgewählt werden kann
   open("kassenauswahl.pl?name="+name.value+"&ik="+ik.value,"kassenwahl","scrollbars=yes,width=600,height=400");
-  };
+};
 
-  function plz_check(plz) {
-//  alert("PLZ"+plz.value);
-  re=/\d{5}/;
-  if (plz.value != '' && !re.test(plz.value)) {
-    alert("Bitte PLZ 5 stellig numerisch erfassen");
-    plz.focus();
-    plz.select();
-  }
-  }
-
-  function set_focus(formular) {
-  var i=formular.length;
-  var y=1;
-  while ( i >= 1 ) {
-    if (undefined != formular.elements[i]) {
-        if (formular.elements[i].value == '') {
-           y = i;
-        }
-    }
-  i--;
-  }
-  formular.elements[y].focus();
-  formular.elements[y].select();
-  }
-  
 function next_satz(formular) {
 	var id = 0;
 	id = new Number(formular.krank_id.value);	
@@ -55,51 +30,3 @@ function prev_satz(formular) {
 		alert("Bitte Menuepunkt Anzeigen wählen");
 	}
 }
-
-
-function auswahl_wechsel (formular) {
-	var wert=formular.auswahl.value;
-	//alert("auswahl wechsel"+wert);
-	switch (wert) {
-	case 'Neu': {
-		//alert("neu");
-		formular.vorheriger.disabled=true;
-		formular.naechster.disabled=true;
-		formular.reset.disabled=false;
-		formular.speichern.disabled=false;
-		formular.speichern.value='Speichern';
-		break;
-		}
-	case 'Ändern': {
-		alert("ändern");
-		formular.vorheriger.disabled=true;
-		formular.naechster.disabled=true;
-		formular.reset.disabled=true;
-		formular.abschicken.disabled=false;
-		formular.abschicken.value='Speichern';
-		//var tag=window.document.getElementsByName("abschicken");	
-		//tag[0].style.visibility="hidden";
-		break;
-		}
-	case 'Anzeigen': {
-		//alert("anzeigen");
-		formular.vorheriger.disabled=false;
-		formular.naechster.disabled=false;
-		formular.reset.disabled=true;
-		formular.abschicken.disabled=true;
-		break;
-		}
-	case 'Löschen': {
-		//alert("loeschen");
-		formular.vorheriger.disabled=true;
-		formular.naechster.disabled=true;
-		formular.reset.disabled=false;
-		formular.abschicken.disabled=false;
-		formular.abschicken.value='Löschen';
-		break;
-		}
-	default: {
-		alert("default");
-		}
-	}
-}		
--- tinyheb/erfassung/stammdaten.js
+++ tinyheb/erfassung/stammdaten.js
@@ -1,56 +1,31 @@
-  function loeschen() {
+function loeschen() {
   open("stammdatenerfassung.pl","_top");
-  };
+};
 
-  function frausuchen(vorname,nachname,geb,formular) {
+function frausuchen(vorname,nachname,geb,formular) {
   // öffnet Fenster in dem eine Frau ausgewählt werden kann
   open("frauenauswahl.pl?vorname="+vorname.value+"&nachname="+nachname.value+"&geb_f="+geb.value+"&suchen=Suchen","frauenwahl","scrollbars=yes,width=700,height=400");
   };
 
-  function datum_check(datum) {
-//    alert("Eingabe"+datum);
-    re=/^\d{1,2}\.\d{1,2}\.\d{1,4}$/;
-    if (datum.value != '' && !re.test(datum.value)) {
-      alert("Bitte Datum im Format tt.mm.jjjj erfassen");
-      datum.focus();
-      datum.select();
-    }
-  }
-
-  function kvnr_check(kvnummer) {
+function kvnr_check(kvnummer) {
   re=/^\d{10}$/;
   if (kvnummer.value != '' && !re.test(kvnummer.value)) {
     alert("Bitte KV-Nummer 10 stellig numerisch erfassen");
     kvnummer.focus();
     kvnummer.select();
   }
-  }
+}
 
-  function plz_check(plz) {
-//  alert("PLZ"+plz.value);
-  re=/\d{5}/;
-  if (plz.value != '' && !re.test(plz.value)) {
-    alert("Bitte PLZ 5 stellig numerisch erfassen");
-    plz.focus();
-    plz.select();
+function kvnr_gueltig_check(kvnr_gueltig) {
+//  alert("gueltig"+kvnr_gueltig.value);
+  re=/^[0-1][0-9]\d{2}$/;
+  if (kvnr_gueltig.value != '' && !re.test(kvnr_gueltig.value)) {
+    alert("Bitte Gültigkeit im Format mmjj erfassen");
+    kvnr_gueltig.focus();
+    kvnr_gueltig.select();
   }
-  }
+}
 
-  function set_focus(formular) {
-  var i=formular.length;
-  var y=0;
-  while ( i >= 0 ) {
-    if (undefined != formular.elements[i]) {
-        if (formular.elements[i].value == '') {
-           y = i;
-        }
-    }
-  i--;
-  }
-  formular.elements[y].focus();
-  formular.elements[y].select();
-  }
-  
   function kassen_auswahl() {
   // öffnet Fenster in dem eine Krankenkasse ausgewählt werden kann
   open("kassenauswahl.pl","kassenwahl","scrollbars=yes,width=600,height=400");
@@ -95,19 +70,6 @@
     opener.document.stammdaten.nicht_naechste_heb.value=begr_nicht_nae_heb;
 }
     
-
-  function kk_eintrag(ik,name,plz,ort,strasse) {
-  // übertragt Krankenkassedaten in die Stammdatenmaske
-
-    // in Parent Dokument übernehmen
-    // alert("parent"+opener.document.forms[0]);
-    var formular = opener.document.stammdaten;
-    formular.ik_krankenkasse.value=ik;
-    formular.name_krankenkasse.value=name;
-    formular.strasse_krankenkasse.value=strasse;
-    formular.ort_krankenkasse.value=plz+' '+ort;
-
-}
 
 function check_begr(wert,formular) {
 // ändert im übergebenen Formular ob Begründung erfasst werden kann oder nicht
