330f0122b1d99da86d0e1f2c447c2fb5db44cd86
Thomas Baum <thomas.baum@arcor.de>
2004-04-18 12:41:36 +0000

    Sonntagsverarbeitung implementiert.

--- tinyheb/erfassung/leistungserfassung_f2.pl
+++ tinyheb/erfassung/leistungserfassung_f2.pl
@@ -33,9 +33,11 @@ my @aus = ('Anzeigen','Ändern','Neu','Löschen');
 
 my $speichern = $q->param('Speichern');
 
-my $datum = $q->param('datum') || $TODAY;
+my $datum = $q->param('datum_leistung');
+$datum = $d->convert($datum) if ($datum ne '');
 my $auswahl = $q->param('auswahl') || 'Anzeigen';
 my $abschicken = $q->param('abschicken');
+my $gruppen_auswahl = $q->param('gruppen_auswahl') || 0;
 my $func = $q->param('func') || 0;
 
 print $q->header ( -type => "text/html", -expires => "-1d");
@@ -61,23 +63,25 @@ print '<body id="leistungen_window_2" bgcolor=white>';
 
 print '<form name="leistungen_f2" action="leistungserfassung_f2.pl" method="get" target=leistungserfassung_f2 bgcolor=white>';
 
-print_table('A');
-print_table('B');
+print_table($gruppen_auswahl);
+
 print "</body>";
 print "</html>";
 
 sub print_table {
-  my ($type) = @_;
-  print '&nbsp;';
-  if ($type eq 'A') {
+  my ($type_ix) = @_;
+  my @type_ar = ('A','B','C','D','W');
+  my $type = $type_ar[$type_ix];
+  print "$type&nbsp;";
+  if ($type_ix == 0) {
     print '<h2>A. Leistungen der Mutterschaftsvorsorge und Schwangerenbetreuung</h2>';
-  } elsif ($type eq 'B') {
+  } elsif ($type_ix == 1) {
     print '<h2>B. Geburtshilfe</h2>';
-  } elsif ($type eq 'C') {
+  } elsif ($type_ix == 2) {
     print '<h2>C. Leistungen während des Wochenbetts</h2>';
-  } elsif ($type eq 'D') {
+  } elsif ($type_ix == 3) {
     print '<h2>D. Sonstige Leistungen</h2>';
-  } elsif ($type eq 'W') {
+  } elsif ($type_ix == 4) {
     print '<h2>Erklärungen zur Gebührenberechnung - Wegegeld</h2>';
   }
   print '<table border="1" width="100%" align="center">';
@@ -91,49 +95,59 @@ sub print_table {
   $l->leistungsart_such($datum,$type);
   while (my @werte = $l->leistungsart_such_next() ) {  
     my $z_art_sonntag="n";
-    if ($werte[6] =~ /^\+/) {
+    my ($posnr,$preis,$prozent) = ($werte[1],$werte[4],$werte[5]);
+    my ($sonntag,$nacht,$samstag) = ($werte[6],$werte[7],$werte[8]);
+    my ($fuerzeit) = ($werte[9]);
+    if ($sonntag =~ /^\+/) {
       $z_art_sonntag = '+';
-      $werte[6] =~ s/\+//;
+      $sonntag =~ s/\+//;
     }
     print "<tr>";
-    print "<td>$werte[1]</td>";
+    print "<td>$posnr</td>";
     print "<td>$werte[2]</td>";
     print <<SCRIPT_KNOPF;
   <td>
-  <input type="checkbox" name="box_name_$werte[1]" id="box_id_$werte[1]" onClick="process_$werte[1]()">
+  <input type="checkbox" name="box_name_$posnr" id="box_id_$posnr" onClick="process_$posnr()">
   </td>
   <script>
-    function process_$werte[1](betrag_ursp) {
-      // alert("Klick auf $werte[1]");
-      // Betrag füllen, wenn gefüllt
-	var tag=window.document.getElementById("td_$werte[1]_betrag");	
-	if (document.leistungen_f2.box_name_$werte[1].checked) {
-          if('$werte[4]' != '0.00') {
-	     tag.firstChild.data="$werte[4]";
+    function process_$posnr(betrag_ursp) {
+      // alert("Klick auf $posnr");
+        var preis=new Number('$preis');
+        var wotag=wo_tag(parent.leistungserfassung_f1.document.leistungen_f1.datum_leistung.value);
+	var tag=window.document.getElementById("td_$posnr\_betrag");	
+
+	if (document.leistungen_f2.box_name_$posnr.checked) {
+          // prüfen, ob Preis von Zeit Abhängig ist
+          if('$fuerzeit' > '0') {
+             preis = zeit_preis($preis,parent.leistungserfassung_f1.document.leistungen_f1.dauer_leistung.value,$fuerzeit);
+          }
+          if(preis > 0) {
+	     tag.firstChild.data=preis;
              tag.firstChild.className="td"; 
            
           }
-          if ('$werte[5]' != '0.00') {
-             var summe = $werte[5];
-             summe = summe * betrag_ursp;
+          if ('$prozent' != '0.00') {
+             summe = round($prozent * betrag_ursp);
  	     tag.firstChild.nodeValue=summe;
           }
 	  // prüfen ob es abhängige Werte gibt
 	  // Sonntags Arbeit
-	    if ( wo_tag(parent.leistungserfassung_f1.document.leistungen_f1.datum_leistung.value)==0 ) {
-	      //alert("Sonntag");
-	      if('$werte[6]' != '0') {
-		alert("zuschlag"+'$werte[6]');
-		document.leistungen_f2.box_name_$werte[6].checked=true;
-		process_$werte[6]($werte[4]); // Original Betrag mitgeben
-                if ('$z_art_sonntag' != '+') {
-                // es handelt sich nicht um Zuschlag, sondern andere Leistung
-                  document.leistungen_f2.box_name_$werte[1].checked=false;
-                  tag.firstChild.nodeValue=" ";
-                }
+	    if ( wotag==0 && '$sonntag' != '0') {
+	      alert("zuschlag"+'$sonntag');
+	      document.leistungen_f2.box_name_$sonntag.checked=true;
+	      process_$sonntag($preis); // Original Betrag mitgeben
+              if ('$z_art_sonntag' != '+') {
+              // es handelt sich nicht um Zuschlag, sondern andere Leistung
+                document.leistungen_f2.box_name_$posnr.checked=false;
+                tag.firstChild.nodeValue="0.00";
 	      }
 	    }
 	} else {
+        // Knopf ist abgeschaltet worden
+          if (wotag==0 && '$sonntag' != '0') {
+            document.leistungen_f2.box_name_$sonntag.checked=false;
+            process_$sonntag(0);
+          }
 	  tag.firstChild.data="0.00";
           tag.firstChild.className='td';
 	}
@@ -141,7 +155,7 @@ sub print_table {
     }
   </script>
 SCRIPT_KNOPF
-    print "<td align=right id='td_$werte[1]_betrag'>&nbsp;</td>\n";
+    print "<td align=right id='td_$posnr\_betrag'>&nbsp;</td>\n";
     print "</tr>\n";
   }
   print "</tbody><br></table>";
