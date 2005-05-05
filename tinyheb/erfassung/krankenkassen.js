513fbb72ac846cb6f3a4b0d7590bf4e3471c0086
Thomas Baum <thomas.baum@arcor.de>
2005-05-05 09:35:10 +0000

    bei suche nach Krankenkasse wird jetzt die aktuelle IK -Aübergeben und -b die n-Aächst größere/ kleinere gesucht.-b

--- tinyheb/erfassung/krankenkassen.js
+++ tinyheb/erfassung/krankenkassen.js
@@ -4,29 +4,28 @@ function loeschen() {
 
 function kassesuchen(name,ik,formular) {
   // öffnet Fenster in dem eine Krankenkasse ausgewählt werden kann
-  open("kassenauswahl.pl?name="+name.value+"&ik="+ik.value,"kassenwahl","scrollbars=yes,width=600,height=400");
+  open("kassenauswahl.pl?name="+name.value+"&ik="+ik.value,"kassenwahl","scrollbars=yes,width=800");
 };
 
 function next_satz(formular) {
-	var id = 0;
-	id = new Number(formular.krank_id.value);	
-	id++;
-//	alert("naechster Satz"+formular+id);
-	if (formular.auswahl.value == 'Anzeigen') {
-		open("krankenkassenerfassung.pl?func=1&krank_id="+id,"_top");
-	} else {
-		alert("Bitte Menuepunkt Anzeigen wählen");
-	}
+  // holt die in ik angegebene Krankenkasse ins Fenster
+  //	alert("naechster Satz"+ik);
+  ik = formular.ik_krankenkasse.value;
+  if (formular.auswahl.value == 'Anzeigen') {
+    open("krankenkassenerfassung.pl?func=1&ik_krankenkasse="+ik,"_top");
+  } else {
+    alert("Bitte Menuepunkt Anzeigen wählen");
+  }
 }
 
+
 function prev_satz(formular) {
-	var id = 0;
-	id = new Number(formular.krank_id.value);	
-	id--;
-//	alert("naechster Satz"+formular+id);
-	if (formular.auswahl.value == 'Anzeigen') {
-		open("krankenkassenerfassung.pl?func=2&krank_id="+id,"_top");
-	} else {
-		alert("Bitte Menuepunkt Anzeigen wählen");
-	}
+  // holt die in ik angegebene Krankenkasse ins Fenster
+  //	alert("naechster Satz"+ik);
+  ik = formular.ik_krankenkasse.value;
+  if (formular.auswahl.value == 'Anzeigen') {
+    open("krankenkassenerfassung.pl?func=2&ik_krankenkasse="+ik,"_top");
+  } else {
+    alert("Bitte Menuepunkt Anzeigen wählen");
+  }
 }
