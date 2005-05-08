c8fc8310c6a4376983c60a7e8dcd148ca4f05892
Thomas Baum <thomas.baum@arcor.de>
2005-05-08 10:33:31 +0000

    Anlegen der Parameter Datenbank.

---
+++ tinyheb/tools/create_table_parms.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl -w
+
+# legt neue Tabelle Stammdaten an
+# alle Stammdaten der Frau
+
+# T. Baum
+# 16.12.03
+
+use strict;
+use DBI;
+
+my $user = 'baum';
+my $pass = '';
+
+my @dsn = ("DBI:mysql:database=Hebamme;host=localhost",$user,$pass);
+
+# mit Datenbank verbinden
+my $dbh = DBI->connect(@dsn,{ RaiseError => 1, AutoCommit => 1 });
+
+# fehler beim verbinden abfangen
+die $DBI::errstr unless $dbh;
+
+# neue Tabelle anlegen
+$dbh->do("CREATE TABLE Parms (" .  
+	 "NAME CHAR(20),".
+	 "VALUE CHAR(100)," .
+	 "BESCHREIBUNG CHAR(100) );") or die $dbh->errstr();
+
+$dbh->disconnect();
