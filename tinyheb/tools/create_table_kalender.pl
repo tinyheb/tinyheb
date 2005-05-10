9c3669e512b51d0c29b5c2fa67e70e12edbd1ec7
Thomas Baum <thomas.baum@arcor.de>
2005-05-10 17:50:50 +0000

    Aufruf der Datenbank -Aüber Heb.pm-b

--- tinyheb/tools/create_table_kalender.pl
+++ tinyheb/tools/create_table_kalender.pl
@@ -2,24 +2,25 @@
 
 # legt neue Tabelle Kalender an
 
+use lib "../";
 use strict;
 use DBI;
 
-my $user = 'baum';
-my $pass = '';
+use Heb;
 
-my @dsn = ("DBI:mysql:database=Hebamme;host=localhost",$user,$pass);
+my $h = new Heb;
 
-# mit Datenbank verbinden
-my $dbh = DBI->connect(@dsn,{ RaiseError => 1, AutoCommit => 1 });
+my $dbh = Heb->connect;
 
 # fehler beim verbinden abfangen
 die $DBI::errstr unless $dbh;
 
 # neue Tabelle anlegen
 $dbh->do("CREATE TABLE Kalender (" .  
+	 "ID SMALLINT UNSIGNED,".
 	 "NAME CHAR(100) NOT NULL," . # Name des Feiertages
 	 "BUNDESLAND CHAR(100) NOT NULL," . # Bundesland in dem der Feiertag gilt
-	 "DATUM DATE NOT NULL);") or die $dbh->errstr(); # Datum des Tages
+	 "DATUM DATE NOT NULL,". # Datum des Feiertages
+	 "PRIMARY KEY(ID) );") or die $dbh->errstr(); # Datum des Tages
 
 $dbh->disconnect();
