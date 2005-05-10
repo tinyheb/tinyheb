6db7995a6dea7f62a5583468ab285f4eeec69e57
Thomas Baum <thomas.baum@arcor.de>
2005-05-10 17:58:47 +0000

    Zugriff auf DB via Heb.pm Es wird Leistungstyp M = Material ber-Aücksichtigt.-b Es gibt Kurzbezeichnung f-Aür Rechnung.-b

--- tinyheb/tools/create_table_leistungsart.pl
+++ tinyheb/tools/create_table_leistungsart.pl
@@ -2,16 +2,13 @@
 
 # legt neue Tabelle Leistungsart an
 
+use lib "../";
 use strict;
 use DBI;
 
-my $user = 'baum';
-my $pass = '';
+use Heb;
 
-my @dsn = ("DBI:mysql:database=Hebamme;host=localhost",$user,$pass);
-
-# mit Datenbank verbinden
-my $dbh = DBI->connect(@dsn,{ RaiseError => 1, AutoCommit => 1 });
+my $dbh = Heb->connect;
 
 # fehler beim verbinden abfangen
 die $DBI::errstr unless $dbh;
@@ -19,10 +16,27 @@ die $DBI::errstr unless $dbh;
 # neue Tabelle anlegen
 $dbh->do("CREATE TABLE Leistungsart (" .  
 	 "ID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT," .
+	 "POSNR CHAR(5) NOT NULL," .
 	 "BEZEICHNUNG TEXT NOT NULL," .
-	 "LEISTUNGSTYP CHAR(02) NOT NULL," .
+	 "LEISTUNGSTYP ENUM ('A','B','C','D','M','W') NOT NULL," .
+	 "EINZELPREIS DECIMAL(6,2),".
+	 "PROZENT DECIMAL (5,2)," .
+	 "SONNTAG CHAR(5) DEFAULT NULL,".
+	 "NACHT CHAR(5) DEFAULT NULL,".
+	 "SAMSTAG CHAR(5) DEFAULT NULL,".
+	 "FUERZEIT TINYINT UNSIGNED NOT NULL DEFAULT 0,".
+	 "DAUER TINYINT UNSIGNED NOT NULL DEFAULT 0,".
+	 "ZWILLINGE CHAR(5) DEFAULT NULL,".
+	 "ZWEITESMAL CHAR(5) DEFAULT NULL,".
+	 "EINMALIG CHAR(5) DEFAULT NULL,".
+	 "BEGRUENDUNGSPFLICHT ENUM ('j','n'),".
+	 "ZUSATZGEBUEHREN1 CHAR(5) DEFAULT NULL,".
+	 "ZUSATZGEBUEHREN2 CHAR(5) DEFAULT NULL,".
+	 "ZUSATZGEBUEHREN3 CHAR(5) DEFAULT NULL,".
+	 "ZUSATZGEBUEHREN4 CHAR(5) DEFAULT NULL,".
 	 "GUELT_VON DATE NOT NULL DEFAULT '1990-01-01'," .
 	 "GUELT_BIS DATE NOT NULL DEFAULT '9999-01-01'," .
+	 "KBEZ CHAR(50), ".
 	 "PRIMARY KEY (ID) );") or die $dbh->errstr();
 
 $dbh->disconnect();
