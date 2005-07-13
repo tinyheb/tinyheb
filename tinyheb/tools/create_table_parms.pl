b91f3e7332058b6215b793da806126a8674ff69b
Thomas Baum <thomas.baum@arcor.de>
2005-07-13 17:42:54 +0000

    Erweiterung der Tabelle um eindeutige ID.

--- tinyheb/tools/create_table_parms.pl
+++ tinyheb/tools/create_table_parms.pl
@@ -21,7 +21,8 @@ my $dbh = DBI->connect(@dsn,{ RaiseError => 1, AutoCommit => 1 });
 die $DBI::errstr unless $dbh;
 
 # neue Tabelle anlegen
-$dbh->do("CREATE TABLE Parms (" .  
+$dbh->do("CREATE TABLE Parms (" .
+	 "ID SMALLINT UNSIGNED NOT NULL,".
 	 "NAME CHAR(20),".
 	 "VALUE CHAR(100)," .
 	 "BESCHREIBUNG CHAR(100) );") or die $dbh->errstr();
