c5090c910bd16b6b4cc4119e2d7765c0e02e32a1
Thomas Baum <thomas.baum@arcor.de>
2005-10-02 07:50:06 +0000

    Die angegebenen Kassen k-Aönnen jetzt gegen den aktuellen Datenbestand -b verglichen werden und bei Bedarf den aktuellen Datenhaushalt -Aüberschreiben.-b Verschiedene Parameter k-Aönnen übergeben werden. Ausgabe dieser Parameter-b mit der option -h.

--- tinyheb/tools/kostentraeger.pl
+++ tinyheb/tools/kostentraeger.pl
@@ -11,16 +11,62 @@
 
 use strict;
 use Date::Calc qw(This_Year Decode_Month Add_Delta_DHMS);
+use Getopt::Std;
+
+my %option = ();
+getopts("vcoundp:f:h",\%option);
+
+if ($option{h}) {
+  print "
+ usage:  $0 options dateien pfad
+ -v <-> debug/verbose
+ -c <-> check
+ -o <-> formatierte ausgabe
+ -u <-> update
+ -n <-> not equal ausgabe ungleiche kassen
+ -d <-> delete (prüfen auf gelöschte kassen)
+ -p <-> path
+ -f <-> file
+ -h <-> help
+";
+  exit;
+}
+use lib "../";
+use Heb_krankenkassen;
+
+my $k = new Heb_krankenkassen;
 
 my $debug = 0;
+my $check = 0; # prüfen gegen Datenbank
+my $ausgabe = 0; # formatierte ausgabe der kassen ein-/ausschalten
+my $update = 0; # soll update auf Datenbank gemacht werden
+my $aus_ungleich =0; # ungleiche KK ausgeben
+my $loeschen=0; # prüfen auf gelöschte kassen
+$debug = 1 if($option{v});
+$check = 1 if($option{c});
+$ausgabe = 1 if($option{o});
+$update = 1 if($option{u});
+$aus_ungleich = 1 if($option{n});
+$loeschen = 1 if($option{d});
 
-my $eingabe = $ARGV[0];
+my $c_gleich = 0; # wie viele kassen sind gleich geblieben
+my $c_ungleich = 0; # wie viele kassen geändert
+my $c_neu = 0; # wie viele kassen neu
+
+my %alle_kassen; # alle in der Datei enthaltenen kassen
+
+my $eingabe = $option{f} || '';
+my $pfad = $option{p} || '';
 
 #$eingabe = 'kostentraeger/'.$eingabe;
 print "Einlesen der Daten von Datei: $eingabe\n" if $debug;
 
+my @dateien = split ' ',$eingabe;
+
+foreach my $file (@dateien) {
+  $file = $pfad.$file;
 # öffnen der Datei mit den Informationen
-open FILE, $eingabe or die "Konnte Datei $eingabe nicht zum Lesen öffnen$!\n";
+open FILE, $file or die "Konnte Datei $file nicht zum Lesen öffnen $!\n";
  
 my $line_counter = 0; 
 my $zeile = '';
@@ -33,6 +79,7 @@ LINE:while ($zeile=<FILE>) {
   next LINE if ($zeile =~ /\AUNB/); # Beginn überspringen
 
   if ($zeile =~ /UNH/) { # Krankenkasse kommt
+    my $kasse=''; # ergebniss hier ablegen
     my $idk =0;
     my $zentral_idk = 0;
     my %ans_haus;
@@ -45,9 +92,14 @@ LINE:while ($zeile=<FILE>) {
     my $bemerkung = ''; # zusätzliche Informationen zur KK
     print "--------------KRANKENKASSE ANFANG\n" if $debug;
     until ($zeile =~ /\AUNT/) {
+      print "ZEILE $zeile\n" if $debug;
       if ($zeile =~ /\AIDK\+(\d{9})\+(\d{2})\+(\D{0,30})/) {
 	$idk = $1;
 	$kname = $3;
+	$kname =~ s/\?\+/ und /g;
+	$kname =~ s/\+/ /g;
+	$kname =~ s/  / /g;
+	$kname =~ s/ $//;
 	print "--> IK: $idk\n" if $debug;
 	print "--> Kurzbezeichnung: $kname\n" if $debug;
       }
@@ -74,6 +126,8 @@ LINE:while ($zeile=<FILE>) {
       }
 
       if ($zeile =~ /\ANAM\+/) {
+	$zeile =~ s/\?\+/ und /g;
+	$zeile =~ s/  / /g;
 	my @erg = split '\+',$zeile;
 	shift @erg;
 	shift @erg;
@@ -94,6 +148,7 @@ LINE:while ($zeile=<FILE>) {
 	  print "Postfachanschrift ort $erg[3]\n" if $debug;
 	  $ans_post{plz}=$erg[2];
 	  $ans_post{ort}=$erg[3];
+	  $erg[4] = '' unless(defined($erg[4]));
 	  $erg[4] =~ s/Postfach//g;
 	  $ans_post{postfach}=$erg[4];
 	}
@@ -111,31 +166,100 @@ LINE:while ($zeile=<FILE>) {
       chomp($zeile);
       $zeile =~ s/\'\x0d//g;
     }
-    print "$idk\t$kname\t$name\t";
-    print $ans_haus{strasse} if defined($ans_haus{strasse});
+    $kasse .= "$idk\t$kname\t$name\t";
+    $kasse .= $ans_haus{strasse} if defined($ans_haus{strasse});
     if (defined($ans_haus{plz})) {
-      print "\t$ans_haus{plz}\t";
+      $kasse .= "\t$ans_haus{plz}\t";
     } else {
-      print "\t\t";
+      $kasse .= "\t\t";
     }
-    print "$ans_post{plz}" if defined($ans_post{plz});
+    $kasse .= "$ans_post{plz}" if defined($ans_post{plz});
     if (defined($ans_haus{ort})) {
-      print "\t$ans_haus{ort}\t";
+      $kasse .= "\t$ans_haus{ort}\t";
     } else {
       if (defined($ans_post{ort})) {
-	print "\t$ans_post{ort}\t";
+	$kasse .= "\t$ans_post{ort}\t";
       } else {
-	print "\t\t";
+	$kasse .= "\t\t";
       }
     }
-    print "$ans_post{postfach}" if defined($ans_post{postfach});
-    print "\t$asp_name\t";
-    print "$asp_tel\t";
-    print "$zentral_idk\t";
-    print "$bemerkung\n";
+    $kasse .=  "$ans_post{postfach}" if defined($ans_post{postfach});
+    $asp_name = '' unless(defined($asp_name));
+    $kasse .= "\t$asp_name\t";
+    $asp_tel = '' unless(defined($asp_tel));
+    $kasse .= "$asp_tel\t";
+    $kasse .= "$zentral_idk\t";
+    $kasse .= "$bemerkung\n";
+    $alle_kassen{$idk}=$kasse;
+    print $kasse if($ausgabe);
     print "--------------KRANKENKASSE ENDE\n" if $debug;
+    my ($k_ik,$k_kname,$k_name,$k_strasse,$k_plz_haus,$k_plz_post,$k_ort,$k_postfach,$k_asp_name,$k_asp_tel,$k_zik,$k_bemerkung)= $k->krankenkassen_krank_ik($idk);
+    if ($check) {
+      my ($ik_n,$kname_n,$name_n,$strasse_n,$plz_haus_n,$plz_post_n,$ort_n,$postfach_n,$asp_name_n,$asp_tel_n,$zik_n,$bemerkung_n) = split "\t",$kasse;
+      $plz_haus_n=0 if ($plz_haus_n eq '');
+      $plz_post_n=0 if ($plz_post_n eq '');
+      $bemerkung_n='' unless (defined($bemerkung_n));
+      if (!defined($k_ik)) {
+	print "Neue Kasse: $kasse";
+	$c_neu++;
+	$k->krankenkassen_ins($idk,$kname_n,$name_n,$strasse_n,$plz_haus_n,$plz_post_n,$ort_n,$postfach_n,$asp_name_n,$asp_tel_n,$zik_n,$bemerkung_n) if ($update);
+      }
+      if (defined($k_ik)) {
+	if ($k_kname eq $kname_n && 
+	    $k_name eq $name_n && 
+	    $k_strasse eq $strasse_n &&
+	    $k_plz_haus == $plz_haus_n &&
+	    $k_plz_post == $plz_post_n &&
+	    $k_ort eq $ort_n &&
+	    $k_postfach eq $postfach_n &&
+	    $k_asp_name eq $asp_name_n &&
+	    $k_asp_tel eq $asp_tel_n  &&
+	    $k_zik eq $zik_n
+	   ) {
+	  $c_gleich++;
+	} else {
+	  $c_ungleich++;
+	  if ($aus_ungleich) {
+	    print "----geaenderte Kasse $k_ik\n";
+	    print "war schon im Datenhaushalt neu aus Datei $file\n" if(defined($alle_kassen{$k_ik}));
+	    print "ALT\t\tNEU\n";
+	    print "NAME $k_name\t\t$name_n\n" if(!($k_name eq $name_n));
+	    print "KNAME $k_kname\t\t$kname_n\n" if(!($k_kname eq $kname_n));
+	    print "$k_strasse\t\t$strasse_n\n" if(!($k_strasse eq $strasse_n));
+	    print "$k_plz_haus\t\t$plz_haus_n\n" if(!($k_plz_haus == $plz_haus_n));
+	    print "$k_plz_post\t\t$plz_post_n\n" if(!($k_plz_post == $plz_post_n));
+	    print "$k_ort\t\t$ort_n\n" if(!($k_ort eq $ort_n));
+	    print "$k_postfach\t\t$postfach_n\n" ;
+	    print "$k_asp_name\t\t$asp_name_n\n";
+	    print "$k_asp_tel\t\t$asp_tel_n\n";
+	    print "$k_zik\t\t$zik_n\n";
+	    print "$k_bemerkung\t\t$bemerkung_n\n";
+	  }
+	  # update auf Datenbank
+	  $k->krankenkassen_update($kname_n,$name_n,$strasse_n,$plz_haus_n,$plz_post_n,$ort_n,$postfach_n,$asp_name_n,$asp_tel_n,$zik_n,$bemerkung_n,$k_ik) if ($update);
+	}
+      }
+    }
   }
 }
 close FILE;
+}
+
+# prüfen ob Krankenkassen nicht mehr existieren
+my $c_geloescht=0;
+if ($loeschen) {
+  my $ik=0;
+  while ($ik = $k->krankenkasse_next_ik($ik)) {
+    if (!defined($alle_kassen{$ik})) {
+      print "Kasse $ik existiert nicht mehr\n";
+      $c_geloescht++;
+    }
+  }
+}
+print "SUMMEN\n";
+print "gleiche Kassen: $c_gleich\n";
+print "ungleiche Kassen: $c_ungleich\n";
+print "neue Kassen: $c_neu\n";
+print "gelöschte Kassen: $c_geloescht\n";
 
 1;
