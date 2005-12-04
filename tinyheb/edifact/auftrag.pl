cad324e53f9ad35abce567d311db3ce0d9e02a49
Thomas Baum <thomas.baum@arcor.de>
2005-12-04 07:35:29 +0000

    neue/ ge-Aänderte Parameter-b bei update wird update auf Datenbank gemacht bei ignore wird ignoriert, ob schon update auf Datenbank gemacht wurde --> es wird erneut update gemacht bei ignore wird ignoriert, on schon edi Rechnung erstellt wurde falls Rechnung schon (Teil-)gezahlt wurde, kann keine update mehr gemacht werden, trotz Parameter ignore

--- tinyheb/edifact/auftrag.pl
+++ tinyheb/edifact/auftrag.pl
@@ -9,24 +9,29 @@ use Getopt::Long;
 use lib '../';
 
 use Heb_Edi;
+use Heb_leistung;
 
 my $debug=1;
 my $e = new Heb_Edi;
+my $l = new Heb_leistung;
 
 my $help=0;
 my $update=0;
 my $sendmail=0;
+my $ignore=0;
 
 my $result = GetOptions('help!' => \$help,
 			'mail!' => \$sendmail,
-			'update!' => \$update);
+			'update!' => \$update,
+			'ignore!' => \$ignore);
 
 if ($help) {
   print "
 usage $0 options rechnr
 --help <-> help
---update <-> kein update/insert der elektronischen Rechnung in Datenbank
---mail <-> automatischen Senden der Rechnung per Email
+--update <-> update/insert der elektronischen Rechnung in Datenbank (default kein update)
+--ignore <-> wenn Rechnung elektronisch gestellt oder Teilgezahlt, wird trotzdem elektronische Rechnung gestellt und ggf. update auf Datenbank gemacht
+--mail <-> automatischen Senden der Rechnung per Email, sonst Ausgabe nach stdout
 ";
   exit;
 }
@@ -35,6 +40,15 @@ my $rechnr = pop @ARGV;
 
 if (!($rechnr =~ /\d{8}/)) {
   die "keine gültige Rechnungsnummer angegeben\n";
+}
+
+$l->rechnung_such("ZAHL_DATUM,BETRAGGEZ,BETRAG,STATUS","RECHNUNGSNR=$rechnr");
+my ($zahl_datum,$betraggez,$betrag,$status)=$l->rechnung_such_next();
+
+die "Rechnung nicht vorhanden Abbruch\n" if (!defined($status));
+
+if ($status > 20 && !($ignore) ) {
+  die "Rechnung wurde schon elektronisch gestellt oder ist schon (Teil-)bezahlt Rechnungsstatus ist:$status\n";
 }
 
 my ($dateiname,$erstell_auf,$erstell_nutz)=$e->edi_rechnung($rechnr);
@@ -50,6 +64,10 @@ if ($sendmail) {
   close SEND;
 } else {
   print $erg;
+}
+if($update) {
+  die "die Rechnung ist schon (Teil-)bezahlt, es kann kein update mehr gemacht werden\n" if($status > 23);
+  $e->edi_update($rechnr,$ignore);
 }
 
 	    
