2101e4093c76545e555b4b772d288447de5e9257
Thomas Baum <thomas.baum@arcor.de>
2005-12-28 18:10:30 +0000

    Neue Felder zur Aufnahme von: PUBLIC KEY der Krankenkasse oder Datenannahmestelle. ZIK_TYP Um welchen Typen Zentral IK handelt es sich, z.B. Datenannahmestelle oder Belegannahmestelle. BELEG_IK IK Nummer an die Belege geschickt werden k-Aönnen, wenn eine solche-b Nummer in der Kostentr-Aägerdatei angegeben ist.-b

---
+++ tools/create_table_krankenkassen.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl -w
+
+# legt neue Tabelle Krankenkassen an
+
+
+use lib "../";
+use strict;
+use DBI;
+
+use Heb;
+
+my $h = new Heb;
+my $dbh = Heb->connect;
+
+# fehler beim verbinden abfangen
+die $DBI::errstr unless $dbh;
+
+# neue Tabelle anlegen
+$dbh->do("CREATE TABLE Krankenkassen (" .  
+	 "IK INT UNSIGNED NOT NULL DEFAULT 0," .
+	 "KNAME CHAR(100)," .
+	 "NAME CHAR(100) NOT NULL,".
+	 "STRASSE TEXT," .
+	 "PLZ_HAUS MEDIUMINT UNSIGNED NOT NULL," .
+	 "PLZ_POST MEDIUMINT UNSIGNED NOT NULL," .
+	 "ORT TEXT," .
+	 "POSTFACH CHAR(10),".
+	 "ASP_NAME CHAR(100),".
+	 "ASP_TEL CHAR(30)," .
+	 "ZIK INT UNSIGNED NOT NULL DEFAULT 0," .
+	 "BEMERKUNG TEXT," .
+	 "PUBKEY MEDIUMBLOB, ".
+	 "ZIK_TYP TINYINT UNSIGNED NOT NULL,".
+	 "BELEG_IK INT UNSIGNED NOT NULL DEFAULT 0,".
+	 "UNIQUE INDEX IK_INDEX(IK),".
+	 "INDEX NAME_INDEX (NAME),".
+	 "PRIMARY KEY (IK) );") or die $dbh->errstr();
+
+$dbh->disconnect();
