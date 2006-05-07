bff2b8e51b9db3a3d5aabccd5ad2fc64e6e4f259
Thomas Baum <thomas.baum@arcor.de>
2006-05-07 17:11:08 +0000

    allgemeine Plausiprüfungen an Möglichkeiten in Firefox 1.5.0.2 angepasst.

--- tinyheb/erfassung/stammdaten.js
+++ tinyheb/erfassung/stammdaten.js
@@ -19,6 +19,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
+// alert('lade stammdaten.js');
 
 function loeschen() {
   open("stammdatenerfassung.pl","_top");
@@ -38,13 +39,41 @@ function frausuchen(vorname,nachname,geb,formular) {
   open("frauenauswahl.pl?vorname="+vorname.value+"&nachname="+nachname.value+"&geb_f="+geb.value+"&suchen=Suchen&sel_status=alle","frauenwahl","scrollbars=yes,width=700,height=400");
   };
 
+function frau_speicher(formular) {
+  // plausiprüfungen bevor formular gespeichert wird.
+  //  alert('check speicher anfang');
+  if (!datum_check(formular.geburtsdatum_frau)) {
+    return false;
+  }
+  if (!datum_check(formular.geburtsdatum_kind)) {
+    return false;
+  }
+  if (!plz_check(formular.plz)) {
+    return false;
+  }
+  if (!kvnr_check(formular.krankenversicherungsnummer)) {
+    return false;
+  }
+  if (!kvnr_gueltig_check(formular.krankenversicherungsnummer_gueltig)) {
+    return false;
+  }
+  if (!ik_gueltig_check(formular.ik_krankenkasse)) {
+    return false;
+  }
+  //alert('check speicher ende');
+  return true;
+}
+
+
 function kvnr_check(kvnummer) {
   re=/^\d{9,10}$/;
   if (kvnummer.value != '' && !re.test(kvnummer.value)) {
     alert("Bitte KV-Nummer 10 stellig numerisch erfassen");
     kvnummer.focus();
     kvnummer.select();
+    return false;
   }
+  return true;
 }
 
 function ik_gueltig_check(ik_nummer) {
@@ -57,14 +86,17 @@ function ik_gueltig_check(ik_nummer) {
       alert("Bitte IK-Nummer 9 stellig numerisch erfassen");
       ik_nummer.focus();
       ik_nummer.select();
+      return false;
     } else {
       ik_nummer.value = '10'+ik_nummer.value;
+      return true;
     }
   }
+  return true;
 }
 
 function kvnr_gueltig_check(kvnr_gueltig) {
-//  alert("gueltig"+kvnr_gueltig.value);
+  //  alert("gueltig"+kvnr_gueltig.value);
   if (kvnr_gueltig.value != '') {
     re=/^([0-1][0-9])\d{2}$/;
     var ret=re.test(kvnr_gueltig.value);
@@ -72,8 +104,10 @@ function kvnr_gueltig_check(kvnr_gueltig) {
       alert("Bitte Gültigkeit im Format mmjj erfassen");
       kvnr_gueltig.focus();
       kvnr_gueltig.select();
+      return false;
     }
   }
+  return true;
 }
 
   function kassen_auswahl() {
@@ -127,3 +161,5 @@ function prev_satz(formular) {
 		alert("Bitte Menuepunkt Anzeigen wählen");
 	}
 }
+
+//alert('stammdaten.js geladen');
