96f64fac8a10a67d0e380635017a74d8ff70ee97
Thomas Baum <thomas.baum@arcor.de>
2006-11-11 17:21:19 +0000

    erste Version

---
+++ win32/ActivePerl_setup.bat
@@ -0,0 +1,23 @@
+@rem = '--*-Perl-*--
+@echo off
+if "%OS%" == "Windows_NT" goto WinNT
+perl -x -S "%0" %1 %2 %3 %4 %5 %6 %7 %8 %9
+goto endofperl
+:WinNT
+perl -x -S %0 %*
+if NOT "%COMSPEC%" == "%SystemRoot%\system32\cmd.exe" goto endofperl
+if %errorlevel% == 9009 echo You do not have Perl in your PATH.
+if errorlevel 1 goto script_failed_so_exit_with_non_zero_val 2>nul
+goto endofperl
+@rem ';
+#!perl
+system('ppm install date-calc');
+system('ppm install dbi');
+system('ppm install DBD-mysql');
+system('ppm install PostScript-Simple');
+system('ppm install Mail-Sender');
+#system('ppm install MIME-tools');
+__END__
+:endofperl
+@echo on
+
