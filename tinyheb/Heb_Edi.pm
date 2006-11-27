04b15b7d0d4c8dd4253c58d5043d93c51004117d
Thomas Baum <thomas.baum@arcor.de>
2006-11-27 17:56:03 +0000

    Bug fix: openssl konnte auf Windows Systemen nicht korrekt aufgerufen werden, wenn openssl nicht in Pfad Variable vorhanden war.

--- tinyheb/Heb_Edi.pm
+++ tinyheb/Heb_Edi.pm
@@ -923,7 +923,7 @@ sub enc {
   }
   if ($schl_flag == 3) {
     # DER verschlüsseln um später base64 encoden zu können
-    open NUTZ, "openssl smime -encrypt -in $path/tmp/$dateiname -des3 -outform DER $path/tmp/zik.pem |" or return ("Konnte Datei nicht DER verschlüsseln",0);
+    open NUTZ, "$openssl smime -encrypt -in $path/tmp/$dateiname -des3 -outform DER $path/tmp/zik.pem |" or return ("Konnte Datei nicht DER verschlüsseln",0);
   }
 
   $dateiname =~ s/\.sig//g;
